7e58c8359506f5de05e22855dac8aee5fc23a7b6
sarva1210 <sarva.vish77@gmail.com>
2026-03-02 22:32:33 +0530

    initialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
-import FaceExpression from './features/Expression/components/FaceExpression'
-
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes" 
+import "./features/shared/styles/global.scss"
 function App() {
   return (
-    <FaceExpression/>
+    <RouterProvider router={router}/>
   )
 }
 
